83be2fb5b34230ea6a8567705dbbb91113cfe2e7
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-15 05:59:46 +0600

    adding

---
+++ src/1.8.ts
@@ -0,0 +1,21 @@
+const userDetails: {
+    id: number,
+    firstName: string,
+    middleName?: string,
+    lastName: string,
+    age: number,
+    address: {
+        city: string,
+        country: string
+    }
+} = {
+    id: 1,
+    firstName: "Amdadul",
+    middleName: "Hossain",
+    lastName: "Rimon",
+    age: 22,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh"
+    }
+}
