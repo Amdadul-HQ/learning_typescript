9eb8250d854c0d189a1306dfbafcb9a809572a3e
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-15 05:33:13 +0600

    adding types

---
+++ src/1.4.ts
@@ -0,0 +1,41 @@
+// string
+const str: string = "Rimon"
+console.log(str);
+
+// number
+const age: number = 22
+console.log(age);
+
+// boolean
+const isMarried: boolean = false
+console.log(isMarried);
+
+// undefined
+
+const x: undefined = undefined;
+
+// null
+
+const y: null = null
+
+// any
+let z: any = "Rimon"
+console.log(z)
+z = 22
+console.log(z)
+z = false
+console.log(z)
+
+// unknown
+let a: unknown = "Rimon"
+console.log(a)
+a = 22
+console.log(a)
+a = false
+console.log(a)
+
+// array
+
+const arr: number[] = [1, 2, 3, 4]
+
+console.log(arr);
