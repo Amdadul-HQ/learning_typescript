8b1965edf1720ab31886d29bb138cf0dcc69ac16
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-15 06:01:17 +0600

    addng

---
+++ src/1.9.ts
@@ -0,0 +1,17 @@
+type Studenst = {
+    name: string,
+    age: number,
+    roll: number,
+    address: string,
+    marks: number,
+    class:string
+}
+
+const studenst1 = {
+    name: "Rimon",
+    age: 22,
+    roll: 1,
+    address: "Dhaka",
+    marks: 80,
+    class: "10"
+}
