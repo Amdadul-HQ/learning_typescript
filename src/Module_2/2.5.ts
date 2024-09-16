24f0496bde6d1755388ba78124bde038452c0ac8
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-16 20:49:06 +0600

    adding

--- src/Module_2/2.5.ts
+++ src/Module_2/2.5.ts
@@ -3,4 +3,18 @@
     const createArry = (value: string) : string[] => {
         return [value]
     }
+    const createArrayWithGeneric = <T>(value: T) : T[] => {
+        return [value]
+    }
+    interface User {
+        name: string,
+        address: string,
+        age: number
+    }
+    const userDetails = createArrayWithGeneric<User>({
+        name: "Rimon",
+        address: "Dhaka",
+        age: 22
+    })
+
 }
