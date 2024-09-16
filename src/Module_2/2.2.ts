ed7c571d9cd937b67b567f918ef132989ef7cc07
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-16 06:49:11 +0600

    adding interface

--- src/Module_2/2.2.ts
+++ src/Module_2/2.2.ts
@@ -45,4 +45,27 @@
         age: 22,
         role: "user"
     }
+
+    type Roll1 = number[]
+
+    const roll: Roll1 = [1, 2, 3, 4, 5]
+    
+    interface Roll2 {
+        [index:number] :number
+    }
+
+    const roll2: Roll2 = [1, 2, 3, 4, 5, 5]
+    
+    type Add = (number1: number, number2: number) => number
+
+    const add: Add = (num1, num2) => num1 + num2;
+    add(1, 2)
+    
+    interface Add2 {
+        (number1: number, number2: number) : number
+    }
+
+    const add2: Add2 = (num1, num2) => num1 + num2
+    add2(2,3)
+    
 }
