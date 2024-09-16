85a50c9840802ec0ec78282809d59caaea76039a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-16 06:53:30 +0600

    adidng generic type

---
+++ src/Module_2/2.3.ts
@@ -0,0 +1,10 @@
+{
+    // generic type
+    const roll: number[] = [1, 2, 3, 4, 5]
+
+    const roll2: Array<number> = [1, 2, 3, 4, 5]
+    
+    const names1: string[] = ["Rimon", "Hossain", "Rafsan"]
+    const names2: Array<string> = ["Rimon", "Hossain", "Rafsan"]
+    
+}
