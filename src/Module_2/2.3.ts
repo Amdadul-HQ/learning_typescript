9c5399fb339d34fbd758c711c6cc0eea8e5deebb
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-16 06:58:42 +0600

    adding generic type

--- src/Module_2/2.3.ts
+++ src/Module_2/2.3.ts
@@ -7,4 +7,13 @@
     const names1: string[] = ["Rimon", "Hossain", "Rafsan"]
     const names2: Array<string> = ["Rimon", "Hossain", "Rafsan"]
     
+    type GenericArray<T> = Array<T>
+
+    const arryNumber: GenericArray<number> = [1, 2, 3, 4, 5]
+
+    const arryString: GenericArray<string> = ["Rimon", "Hossain", "Rafsan"]
+
+    const arryBool: GenericArray<boolean> = [true, false, true]
+    
+
 }
