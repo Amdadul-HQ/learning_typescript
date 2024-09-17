ff91bd80bbdfc857b78fa5262c12330d72c894ca
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-17 06:26:57 +0600

    adding promise

--- src/Module_2/2.8.ts
+++ src/Module_2/2.8.ts
@@ -1,5 +1,21 @@
 {
     // Promise
+    interface TODO {
+        id: number,
+        userId: number,
+        title: string,
+        completed: boolean
+    }
+
+    const getToDo = async () : Promise<TODO> => {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos/1"
+        );
+        const data = await response.json();
+        console.log(data);
+        return data
+    }
+    getToDo()
 
     const createPromise = () => {
         const data :string = "something"
@@ -17,5 +33,5 @@
         const result = await createPromise();
         console.log(result)
     }
-    showdata();
+    // showdata();
 }
---
+++ src/Module_2/2.9.ts
@@ -0,0 +1,6 @@
+{
+
+    // conditional types
+
+    
+}
