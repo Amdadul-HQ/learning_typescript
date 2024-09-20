d26ad4879d37750766fb613b60791ff391b85f0a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-20 14:34:15 +0600

    adding geniric array

--- src/Module_2/2.9.ts
+++ src/Module_2/2.9.ts
@@ -14,7 +14,16 @@
     const rimon: Vechials = "car"
     
     const rimonhas: Vechials = "bike"
+
+    type T = {
+        name: string,
+        age: number,
+        address: string,
+    }
     
+    const createGeniricArray = <T>(value : T) : T[] => {
+        return [value]
+    }
     
 
 }
