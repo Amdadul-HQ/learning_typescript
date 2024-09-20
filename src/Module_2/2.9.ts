d94b0e950b2b11e6b45b89e6c659ab9a8568f526
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-20 14:45:12 +0600

    adding conditional types

--- src/Module_2/2.9.ts
+++ src/Module_2/2.9.ts
@@ -9,6 +9,19 @@
     type A3 = A1 extends null ? true : false
 
     type A4 = A1 extends null ? true : A2 extends undefined ? undefined : any
+
+    type Shek = {
+        bike: string,
+        car: string,
+        ship: string,
+    }
+
+    type CheckVichials<T> = T extends keyof Shek ? true : false
+
+    type HasBike = CheckVichials<"bike">
+
+    type HasPlane = CheckVichials<"plane">
+    
     
 
 }
