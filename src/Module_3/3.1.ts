85e2348edb25a320baf1a7c75b82080e68a21d3a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-22 05:46:22 +0600

    adding class function

---
+++ src/Module_3/3.1.ts
@@ -0,0 +1,22 @@
+{
+    // 
+    class Animal {
+        name: string;
+        spices: string;
+        sound: string;
+
+        constructor(name: string, spices: string, sound: string) {
+            this.name = name;
+            this.spices = spices;
+            this.sound = sound;
+        }
+
+        makeSound() {
+            console.log(`The ${this.spices} name is ${this.name}.And says ${this.sound}`);
+        }
+    }
+
+
+    const dog = new Animal("Tiger", "Bag", "Ghew Ghew")
+    dog.makeSound()
+}
