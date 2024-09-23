b6391f4c3535b9b20a4036e7b6dfcbfdff099d00
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-23 19:58:38 +0600

    adding type guard

--- src/Module_3/3.3.ts
+++ src/Module_3/3.3.ts
@@ -21,4 +21,47 @@
     console.log(result);
     console.log(result2);
 
+    interface  NormalUser{
+        name:string
+    }
+    interface AdminUser {
+        name:string
+        role:string
+    }
+
+    const normalUser: NormalUser = {name:"Rimon"}
+    const getRole : NormalUser | AdminUser = (user: AdminUser | NormalUser) => {
+        if ('role' in user) {
+            console.log(`${user.name} is a ${user.role}`);
+        }
+        else {
+            console.log(`${user.name} is a normal user`);
+        }
+    }
+
+    const adminUser = {
+        name: "Rimon",
+        role: "admin"
+    }
+    getRole(adminUser)
+
+
+
+
+
+    // const user = (param : NormalUser | AdminUser) : NormalUser | AdminUser => {
+    //     if ('role' in param) {
+    //         console.log(`${param.name} is a ${param.role}`);
+    //     } else {
+    //         console.log(`${param.name} is a normal user`);
+    //     }
+    // }
+
+    // const result3 = user({name:"Rimon", role:"admin"})
+    // user(result3)
+
+
+
+
+
 }
