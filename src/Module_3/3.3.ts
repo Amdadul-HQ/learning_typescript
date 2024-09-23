05178b2e881d1eea146e5f07b5e19f8a7d745d23
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-23 19:47:04 +0600

    adding type guard

--- src/Module_3/3.3.ts
+++ src/Module_3/3.3.ts
@@ -1,6 +1,24 @@
 {
     // type guard
+    type Jkonoman = string | number
+    const add = (
+        param1: Jkonoman,
+        param2: Jkonoman
+    ): Jkonoman => {
+        if(typeof param1 === "string" || typeof param2 === "string") {
+            return `${param1}${param2}`
+        }
+        else {
+            return param1 + param2
+        }
+    }
 
-    
+    const result = add(3, 4)
+
+    const result2 = add("Rimon", "Hossain")
+
+
+    console.log(result);
+    // console.log();
 
 }
