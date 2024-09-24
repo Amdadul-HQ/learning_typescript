21eadb4e07871c5e471d2ecde81b1b1990660adf
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-25 05:17:42 +0600

    object oriented instance of gaurd

--- src/Module_3/3.4.ts
+++ src/Module_3/3.4.ts
@@ -1,6 +1,37 @@
 {
 
     // instance of gaurd
-    
+    class Animal {
+        name: string;
+        spices: string;
+
+        constructor(name: string, spices: string) {
+            this.name = name;
+            this.spices = spices;   
+        }
+
+        makeSound() {
+            console.log('I am making sound');
+        }
+    }
+
+    class Dog extends Animal {
+        constructor(name: string, spices: string) {
+            super(name, spices);
+        }
+         
+        makeBark() {
+            console.log("I am barking");
+        }
+    }
+    class Cat extends Animal {
+        constructor(name: string, spices: string) {
+            super(name, spices);
+        }
+         
+        makeMeaw() {
+            console.log("I am meawing");
+        }
+    }
 
 }
