d26a0d14e53c4a219d8e427303e027b51d899348
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-25 05:54:27 +0600

    static properties

---
+++ src/Module_3/3.7.ts
@@ -0,0 +1,17 @@
+{
+    // static properties
+    
+    class Counter {
+        count: number = 0;
+        increment() {
+            this.count += 1
+        }
+        decrement() {
+            this.count -= 1
+        }
+
+    }
+
+
+
+}
