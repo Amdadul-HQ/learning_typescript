a37d385049121f239b1ecc4b590a589ac1fa7555
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-25 05:32:54 +0600

    Access Modifiers

--- src/Module_3/3.5.ts
+++ src/Module_3/3.5.ts
@@ -1,6 +1,17 @@
 {
     // Access Modifiers
 
-
+    class BankAccount{
+        readonly id: number;
+        name: string;
+        balance: number;
+        constructor(id: number, name: string, balance: number){
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+    }
+    const goriberAccount = new BankAccount(1, "Goriber", 1000);
+    // goriberAccount.name = "rimon";
 
 }
