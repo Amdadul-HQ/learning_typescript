c8bf58083cda16477ef47aa6b37aff114bb91fff
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-25 05:41:27 +0600

    Access Modifiers

--- src/Module_3/3.5.ts
+++ src/Module_3/3.5.ts
@@ -4,19 +4,33 @@
     class BankAccount{
         readonly id: number;
         public name: string;
-        private balance: number;
-        constructor(id: number, name: string, balance: number){
+        protected _balance: number;
+        private password: number;
+        constructor(id: number, name: string, _balance: number){
             this.id = id;
             this.name = name;
-            this.balance = balance;
+            this._balance = _balance;
         }
         addDiposit(amount: number){
-            this.balance += amount;
-            console.log(`Deposited ${amount}, available balance is ${this.balance}`);
+            this._balance += amount;
+            console.log(`Deposited ${amount}, available balance is ${this._balance}`);
+        }
+        get_Balance(){
+            console.log(`Available balance is ${this._balance}`);
         }
     }
+
+    class StudentAccount extends BankAccount{
+        
+        test() {
+            this._balance = 100
+        }
+    }
+
+
     const goriberAccount = new BankAccount(1, "Goriber", 1000);
     goriberAccount.addDiposit(500);
+    goriberAccount.getBalance();
 
 
 }
