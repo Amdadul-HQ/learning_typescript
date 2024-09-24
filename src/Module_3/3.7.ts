9056d095716987822db8d7e2efb5a84caf1997fa
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-25 05:58:59 +0600

    static properties

--- src/Module_3/3.7.ts
+++ src/Module_3/3.7.ts
@@ -2,15 +2,22 @@
     // static properties
     
     class Counter {
-        count: number = 0;
+        static count: number = 0;
         increment() {
-            this.count += 1
+           return (Counter.count += 1);
         }
         decrement() {
-            this.count -= 1
+          return (Counter.count -= 1);
         }
 
     }
+
+    const instance1 = new Counter();
+    console.log(instance1.increment());
+    const instance2 = new Counter();
+    console.log(instance2.increment());
+    const instance3 = new Counter();
+    console.log(instance3.increment());
 
 
 
