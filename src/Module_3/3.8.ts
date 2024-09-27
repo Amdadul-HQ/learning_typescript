993b990fd98398fba226e3f428bab9812cae504a
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-27 21:57:20 +0600

    adding shape function

--- src/Module_3/3.8.ts
+++ src/Module_3/3.8.ts
@@ -29,4 +29,35 @@
     getSleepingHoure(person3)
 
 
+    class Shape {
+        getArea():number {
+            return 0
+        }
+    }
+
+    class Circle extends Shape {
+        radius: number
+        constructor(radius: number) {
+            super();
+            this.radius = radius
+        }
+        getArea(): number {
+            return Math.PI * this.radius * this.radius
+        }
+    }
+
+    class Rectangle extends Shape {
+        height: number
+        width: number
+        constructor(height: number, width: number) {
+            super();
+            this.height = height
+            this.width = width
+        }
+        getArea(): number {
+            return this.height * this.width
+        }
+    }
+
+
 }
