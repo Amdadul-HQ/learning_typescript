05ba11c95afd4a097b2b1490d77621b7ee65404d
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-09-28 17:30:12 +0600

    adding abstraction

--- src/Module_3/3.9.ts
+++ src/Module_3/3.9.ts
@@ -1,5 +1,17 @@
 {
     // abstraction
-    
+    // Two way to implement abstraction
+    // 1. Interface
+    // 2. Abstract Class
+    interface Vhicle1 {
+        name: string;
+        model: number
+    }
 
+    class Car implements Vhicle1 {
+        name: string
+        constructor(name: string) {
+            this.name = name
+        }
+    }
 }
