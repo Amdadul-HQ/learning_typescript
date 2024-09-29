7bd4bd2223199ab152be70a8dfc0ac8b1edaac52
Amdadul-hQ <rimonamdadul301@gmail.com>
2024-09-29 20:36:52 +0600

    adding problem 1 assigemnet

--- src/Module_3/3.10.ts
+++ src/Module_3/3.10.ts
@@ -1,3 +1,39 @@
 {
-    // 
+    //
+      // getter and setter
+      class BankAccount {
+        readonly id: number;
+        public name: string;
+        protected _balance: number;
+        constructor(id: number, name: string, _balance: number) {
+          this.id = id;
+          this.name = name;
+          this._balance = _balance;
+        }
+        get balance() {
+          return console.log(`Available balance is ${this._balance}`);
+        }
+        set diposit(amount: number) {
+          console.log(
+            `Deposited ${amount}, available balance is ${
+              this._balance + amount
+            }`
+          );
+        }
+        //   addDiposit(amount: number) {
+        //     this._balance += amount;
+        //     console.log(
+        //       `Deposited ${amount}, available balance is ${this._balance}`
+        //     );
+        //   }
+        //   get_Balance() {
+        //     console.log(`Available balance is ${this._balance}`);
+        //   }
+      }
+
+      const goriberAccount = new BankAccount(1, "Goriber", 1000);
+
+      console.log(goriberAccount.balance);
+      goriberAccount.diposit = 1000;
+    
 }
---
+++ src/Module_4/Problem1.ts
@@ -0,0 +1,19 @@
+{
+//     Problem 1:
+// Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+
+// // Sample Input: 
+// repeatString("Hello!", 3)
+
+// // Sample Output:
+    // "Hello!Hello!Hello!"
+
+    type IRepeatString = (str:string,num:number) => string
+
+    const repeatString: IRepeatString = (str:string,num:number) => {
+        return str.repeat(num)
+    }   
+
+    console.log(repeatString("Hello!", 3));
+
+}
